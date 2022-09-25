343614801f380e908c319950762a9ebb516ae4ad
lp84 <lp84@inbox.ru>
2022-09-25 07:06:50 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import Header from "./components/Header.js"
+import Users from "./components/users.js"
+import AddUser from "./components/addUser.js"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            users: [
+                {
+                    id: 1,
+                    firstName: "Bob",
+                    lastName: "Marley",
+                    bio: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Natus, autem?",
+                    age: 40,
+                    isHappy: true
+                },
+                {
+                    id: 2,
+                    firstName: "John",
+                    lastName: "Doe",
+                    bio: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Natus, autem?",
+                    age: 22,
+                    isHappy: false
+                }
+            ]
+        }
+        this.addUser = this.addUser.bind(this)
+    }
+    render() {
+        return (<div>
+            <Header title="list of users" />
+            <main>
+                <Users users={this.state.users} />
+            </main>
+            <aside>
+                <AddUser onAdd={this.addUser} />
+            </aside>
+
+        </div>
+        )
+    }
+
+    addUser(user) {
+        const id = this.state.users.length + 1
+        this.setState({ users: [...this.state.users, { id, ...user }] })
+    }
+
 }
-
-export default App;
+export default App
